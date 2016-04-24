eb597473774d74f4838a13bd124b9157ffa72951
Maximilian Klein <dasmaxl@gmail.com>
2016-04-24 22:48:46 +0200

    (feature) Support lambda functions in go.

--- src/cli.js
+++ src/cli.js
@@ -53,10 +53,29 @@ program
     .then((res) => resolveLambdaTypes(res))
     .then((res) => remodelPorts(res))
     .then((res) => replaceGenerics(res))
-//    .then((res) => gogen.preprocess(res))
-//    .then((res) => gogen.generateCode(res))
+    .then((res) => gogen.preprocess(res))
+    .then((res) => gogen.generateCode(res))
+//    .then((res) => console.log(JSON.stringify(graphlib.json.write(res), null, 2)))
+    .then((res) => console.log(res))
+    .catch((err) => {
+      console.error('error while transpiling')
+      console.error(err.stack)
+    })
+  })
+
+program
+  .command('ng <json>')
+  .option('-o, --output <outputFile>', 'The output filename to generate')
+  .description('Compile a program description into a program using a specific language.')
+  .action((json, options) => {
+    var client = lib(program.elastic)
+    resolve(graphlib.json.read(JSON.parse(fs.readFileSync(json, 'utf8'))), client.get)
+    .then((res) => normalize(res))
+    .then((res) => applyTypings(res, {number: 'int64', bool: 'bool', string: 'string'}))
+    .then((res) => resolveLambdaTypes(res))
+    .then((res) => remodelPorts(res))
+    .then((res) => replaceGenerics(res))
     .then((res) => console.log(JSON.stringify(graphlib.json.write(res), null, 2)))
-//    .then((res) => console.log(res))
     .catch((err) => {
       console.error('error while transpiling')
       console.error(err.stack)
@@ -73,22 +92,6 @@ program
     .then((res) => normalize(res))
     .then((res) => console.log(JSON.stringify(graphlib.json.write(res))))
     .catch((err) => console.error(err.stack))
-  })
-
-program
-  .command('ng <json>')
-  .option('-o, --output <outputFile>', 'The output filename to generate')
-  .description('Compile a program description into a program using a specific language.')
-  .action((json, language, options) => {
-    var client = lib(program.elastic)
-    resolve(graphlib.json.read(JSON.parse(fs.readFileSync(json, 'utf8'))), client.get)
-    .then((res) => normalize(res))
-    .then((res) => remodelPorts(res))
-    .then((res) => console.log(JSON.stringify(graphlib.json.write(res))))
-    .catch((err) => {
-      console.error('error while transpiling')
-      console.error(err.stack)
-    })
   })
 
 program.parse(process.argv)
