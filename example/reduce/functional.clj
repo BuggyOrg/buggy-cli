e97997864403e7441055978390b1f6700eb9957b
Maximilian Klein <dasmaxl@gmail.com>
2016-05-23 18:18:26 +0200

    (fix) No closures in clojure.

--- example/reduce/functional.clj
+++ example/reduce/functional.clj
@@ -7,10 +7,10 @@
     (array/empty list)))
 
 (defco filter [list fn]
-  (fold list (lambda (acc cur)
+  (fold list (functional/partial_n fn 0 (lambda (fn acc cur)
     (logic/mux
       (array/append acc cur)
       acc
-      (functional/apply fn cur))) []))
+      (functional/apply fn cur)))) []))
 
 (io/stdout (translator/array_to_string (filter (translator/string_to_array (io/stdin)) (lambda (n) (math/less n 10)))))
