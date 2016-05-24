885662a7ddcbc66f4454fdcdfe57762b6e8b9220
Maximilian Klein <dasmaxl@gmail.com>
2016-05-24 12:16:31 +0200

    (fix) Use partials correctly.

--- example/reduce/functional.clj
+++ example/reduce/functional.clj
@@ -3,14 +3,14 @@
 (defco fold [list fn init]
   (logic/mux
     list
-    (functional/apply (functional/partial_n fn 1 (array/first list)) (fold (array/rest list) fn init))
+    (functional/apply (functional/partial fn (array/first list)) (fold (array/rest list) fn init))
     (array/empty list)))
 
 (defco filter [list fn]
-  (fold list (functional/partial_n fn 0 (lambda (fn acc cur)
+  (fold list (functional/partial (lambda (fn acc cur)
     (logic/mux
       (array/append acc cur)
       acc
-      (functional/apply fn cur)))) []))
+      (functional/apply fn cur))) fn) []))
 
 (io/stdout (translator/array_to_string (filter (translator/string_to_array (io/stdin)) (lambda (n) (math/less n 10)))))
