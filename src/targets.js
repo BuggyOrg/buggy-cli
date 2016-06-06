c4c3565c30b1c71ec2bed63c9bd636ddb02b5816
Maximilian Klein <dasmaxl@gmail.com>
2016-06-06 20:47:13 +0200

    (feature) Start working on api interface.

---
+++ src/targets.js
@@ -0,0 +1,15 @@
+
+export const json = {
+  name: 'json',
+  description: 'JSON files in canonical form with no resolved nodes.'
+}
+
+export const interactive = {
+  name: 'interactive',
+  description: 'An interactive display of the graph in a browser.'
+}
+
+export const compiled = {
+  name: 'compile',
+  description: 'Executable code in a programming language.'
+}
---
+++ src/transformations.js
@@ -0,0 +1,9 @@
+
+import {resolve} from '@buggyorg/resolve'
+
+export const jsonToResolved = {
+  from: 'json',
+  to: 'resolved',
+  needsComponentLibrary: true,
+  process: resolve
+}
