2ef45533ab03f008274e8d0c224418c8e71106ec
Maximilian Klein <dasmaxl@gmail.com>
2016-12-14 11:26:12 +0100

    (fix) Do not demand "to" for library.

--- src/cli/library.js
+++ src/cli/library.js
@@ -4,9 +4,12 @@ import library from '@buggyorg/library-cli/lib/commands/commands'
 export const command = 'library'
 export const description = 'Library functionalities'
 export const builder = (yargs) => {
+  // hacky-di-hack remove demand for t
+  delete yargs.getDemanded()['t']
   // add commands from library-client
   return library(yargs)
 }
 
 export const handler = (argv) => {
+  global.wasCommand = true
 }
