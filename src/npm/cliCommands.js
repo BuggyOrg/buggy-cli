4bd6ca6d3a122e7ffdcd7abe02616b8583b8ad6d
Maik Marschner <mmarschner@gdv.uni-hannover.de>
2017-03-10 19:03:28 +0100

    (refactor) Implement cliCommands with async und await.

--- src/npm/cliCommands.js
+++ src/npm/cliCommands.js
@@ -22,20 +22,20 @@ const deRange = (versionRange) => {
  * @returns {Promise} If the package was successfully installed the promise will resolve otherwise it will
  *   reject.
  */
-export const install = (dependency, version, path) => {
-  var opts = {cwd: path, env: merge(process.env, {NODE_ENV: 'production'})}
-  return mkdirp(path)
-  .then(() => exec(`npm pack ${dependency}@${version} -q`, opts))
-  .then((res) => res.stdout.trim())
-  .then((tar) => exec('tar -xzf ' + tar + ' --strip-components=1 package', opts).then(() => tar))
-  .then((tar) => exec('rm ' + tar, opts))
-  .then(() => exec('npm i', opts))
+export async function install (dependency, version, path) {
+  var opts = { cwd: path, env: merge(process.env, {NODE_ENV: 'production'}) }
+  await mkdirp(path)
+  const res = await exec(`npm pack ${dependency}@${version} -q`, opts)
+  const tar = res.stdout.trim()
+  await exec('tar -xzf ' + tar + ' --strip-components=1 package', opts)
+  await Promise.all([exec('rm ' + tar, opts), exec('npm i', opts)])
 }
 
-export const cliInterface = (pkg, version, path) =>
-  exec('npm view ' + pkg + ((version) ? ('@' + version) : '') + ' bin --json')
-  .then((res) => JSON.parse(res.stdout))
-  .then((bins) => join(path, bins[Object.keys(bins)[0]])) // rather ugly.. there is no inherent order in an object... so this could be random
+export async function cliInterface (pkg, version, path) {
+  const res = await exec('npm view ' + pkg + ((version) ? ('@' + version) : '') + ' bin --json')
+  const bins = JSON.parse(res.stdout)
+  return join(path, bins[Object.keys(bins)[0]]) // rather ugly.. there is no inherent order in an object... so this could be random
+}
 
 /**
  * Get the version of a dependency for a specific package. Returns null if it
@@ -46,18 +46,19 @@ export const cliInterface = (pkg, version, path) =>
  * @returns {String} A Semver string specifying the version of the dependency. If
  * the package does not depend on the dependency it will return null.
  */
-export const dependencyVersion = (pkg, pkgVersion, dependency) =>
-  exec('npm view ' + pkg + ((pkgVersion) ? ('@' + pkgVersion) : '') + ' dependencies.' + dependency + ' --json')
-  .then((dependency) => {
-    if (/undefined/.test(dependency.stdout) || dependency.stdout.length < 2) return null
-    return deRange(dependency.stdout.slice(1, -2))
-  })
+export async function dependencyVersion = (pkg, pkgVersion, dependency) {
+  const dependency = await exec('npm view ' + pkg + ((pkgVersion) ? ('@' + pkgVersion) : '') + ' dependencies.' + dependency + ' --json')
+  if (/undefined/.test(dependency.stdout) || dependency.stdout.length < 2)
+    return null
+  return deRange(dependency.stdout.slice(1, -2))
+}
 
 /**
  * Gets a list of versions for a package.
  * @param {String} pkg The package.
  * @returns {Array} An array of version strings (in Semver format).
  */
-export const packageVersions = (pkg) =>
-  exec('npm view ' + pkg + ' versions --json')
-  .then((versions) => JSON.parse(versions.stdout))
+export async function packageVersions (pkg) {
+  const versions = await exec('npm view ' + pkg + ' versions --json')
+  return JSON.parse(versions.stdout)
+}
